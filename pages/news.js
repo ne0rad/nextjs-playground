47e489f404c5262cdb8065585ca337bbe7c17682
ne0rad <usackasr@gmail.com>
2022-03-31 21:54:25 +0100

    Updated fetching method

--- pages/news.js
+++ pages/news.js
@@ -1,41 +1,44 @@
+import { useState, useEffect } from "react"
 import Card from "../components/Card"
 import Grid from "../components/Grid"
 import Text from "../components/Text"
 import Title from "../components/Title"
 import { API_URL } from "../config"
 
-export default function News({ news }) {
+export default function News() {
+
+    const [data, setData] = useState(null)
+    const [isLoading, setLoading] = useState(true)
+
+    useEffect(() => {
+        fetch(`${API_URL}/api/news`)
+            .then((res) => res.json())
+            .then((data) => {
+                setData(data.articles)
+                setLoading(false)
+            })
+    }, [])
 
     return (
         <>
             <Title>News</Title>
             <Text variant='title'>Daily News</Text>
-            <Grid>
-                {news.map(({ title, description, url, urlToImage }, i) => {
-                    if (!urlToImage) return;
-                    return (
-                        <Card
-                            key={i}
-                            title={title}
-                            description={description}
-                            url={url}
-                            imageUrl={urlToImage}
-                        />
-                    )
-                })}
-            </Grid>
+            {isLoading ? <Text variant='description'>Loading...</Text> :
+                <Grid>
+                    {data.length > 0 && data.map(({ title, description, url, urlToImage }, i) => {
+                        if (!urlToImage) return;
+                        return (
+                            <Card
+                                key={i}
+                                title={title}
+                                description={description}
+                                url={url}
+                                imageUrl={urlToImage}
+                            />
+                        )
+                    })}
+                </Grid>
+            }
         </>
     )
-}
-
-export async function getStaticProps() {
-
-    const res = await fetch(`${API_URL}/api/news`)
-    const data = await res.json()
-
-    return {
-        props: {
-            news: data.articles
-        }
-    }
 }
